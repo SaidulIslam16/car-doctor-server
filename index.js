9f8a0a4f92a564635e4d7f30e31a959f731afd35
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-08-09 17:31:00 +0600

    individual service api created

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -27,12 +27,32 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
-        const servicesCollection = client.db('carDoctor').collection('services')
+        const servicesCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
 
         app.get('/services', async (req, res) => {
             const cursor = servicesCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, price: 1, service_id: 1 },
+            };
+
+            const result = await servicesCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        // Bookings
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
         })
 
         // Send a ping to confirm a successful connection
